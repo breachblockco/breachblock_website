c122b3b209e8a5ff71a10355d38cdbd4e176fe65
breachblockco <breachblockco@gmail.com>
2025-01-07 01:57:51 +0530

    course details page responsive done

--- client/src/pages/CourseDetails/CourseContentAccordion.jsx
+++ client/src/pages/CourseDetails/CourseContentAccordion.jsx
@@ -12,12 +12,12 @@ function CourseContentAccordion({courseContent,key}) {
       className={`w-full bg-transparent border-t-2 border-[#232323] overflow-hidden mx-auto `}
     >
       <button
-        className={`flex items-center justify-between text-neutral-200 w-full text-left my-8 ${
+        className={`flex items-center justify-between text-neutral-200 w-full text-left my-4 sm:my-8 ${
           isOpen ? "bg-neutral-800/40</div>" : ""
         }`}
         onClick={toggleAccordion}
       >
-        <span className="text-4xl font-medium leading-50">
+        <span className="text-lg sm:text-4xl font-medium leading-50 w-[90%]">
           {courseContent.title}
         </span>
         <motion.span
@@ -26,7 +26,7 @@ function CourseContentAccordion({courseContent,key}) {
           transition={{ duration: 0.4, linear: [0.87, 0, 0.13, 1] }}
         >
           <IoIosArrowDown
-            className={`w-10 h-10 ${
+            className={`w-6 h-6 sm:w-10 sm:h-10 ${
               isOpen ? "text-[#096CD5]" : "text-neutral-100"
             }`}
           />
@@ -44,7 +44,7 @@ function CourseContentAccordion({courseContent,key}) {
             {courseContent.subtopics.map((subtopic, index) => (
               <div
                 key={index}
-                className="p-4 text-slate-300 text-3xl hover:text-[#096CD5] transition-all duration-300"
+                className="p-1 sm:p-4 text-slate-300 text-md sm:text-3xl hover:text-[#096CD5] transition-all duration-300"
               >
                 {subtopic}
               </div>
